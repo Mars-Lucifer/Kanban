d904be56b31e9cfc806576ee498f53fc072934fa
Anaraket <tulinovartem4@gmail.com>
2024-11-06 18:57:12 +0300

    back old

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+"""
+This module handles the main functionality for the Pokemon app.
+"""
+import os
+import json
 from flask import Flask, request, render_template
 
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return render_template('/index.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('/resume.html')
+
+@app.route('/create')
+def create():
+    return render_template('/create.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -1,17 +1,42 @@
-function opens() {
-    const menu = document.getElementById("hamburger");
+// Функция для отображения текущего времени и даты
+function updateCurrentDateTime() {
+    const timeElement = document.getElementById('currentTime');
+    const dateElement = document.getElementById('currentDate');
 
-    menu.style.display = 'flex';
-    setTimeout(() => {
-        menu.classList.add("show");
-    }, 10);
+    const currentTime = new Date();
+    const hours = String(currentTime.getHours()).padStart(2, '0'); // Получаем часы
+    const minutes = String(currentTime.getMinutes()).padStart(2, '0'); // Получаем минуты
+
+    // Форматируем дату
+    const options = { weekday: 'long', day: 'numeric', month: 'long' };
+    const formattedDate = currentTime.toLocaleDateString('ru-RU', options).split(', ');
+
+    // Приводим день недели к заглавной букве
+    const dayOfWeek = formattedDate[0].charAt(0).toUpperCase() + formattedDate[0].slice(1);
+
+    timeElement.textContent = `${hours}:${minutes}`; // Обновляем текст элемента только с часами и минутами
+    dateElement.innerHTML = `<p>${dayOfWeek}</p><p>${formattedDate[1]}</p>`; // Обновляем текст элемента с датой
 }
 
-function closes() {
-    const menu = document.getElementById("hamburger");
+// Обновляем время и дату каждую секунду
+setInterval(updateCurrentDateTime, 1000);
 
-    menu.classList.remove("show");
-    setTimeout(() => {
-        menu.style.display = 'none';
-    }, 500);
+// Вызываем функцию сразу, чтобы не ждать первую секунду
+updateCurrentDateTime();
+
+// Ширина для sidebar
+const parent = document.querySelector('.sidebar__space');
+const child = document.querySelector('.sidebar');
+
+function updateParentWidth() {
+  const childWidth = child.getBoundingClientRect().width;
+  const parentPadding = 
+    parseFloat(getComputedStyle(parent).paddingLeft) + 
+    parseFloat(getComputedStyle(parent).paddingRight);
+  
+  parent.style.width = `${childWidth + parentPadding}px`;
 }
+
+// Вызов при загрузке и обновлении
+updateParentWidth();
+window.addEventListener('resize', updateParentWidth);
